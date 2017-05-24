4d97c0c5177a418b352516b053f1dc028db52a04
Sara Robinson <sararob@google.com>
2017-05-24 17:23:43 -0400

    Update Cloud Function to use google-cloud-node for Video API call

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const request = require('request');
 const config = require('./local.json')
 
 const storage = require('@google-cloud/storage');
@@ -21,8 +20,14 @@ const storageClient = storage({
   keyfileName: "keyfile.json"
 });
 
+const video = require('@google-cloud/video-intelligence');
+const videoClient = video({
+  projectId: config.cloud_project_id,
+  keyfileName: "keyfile.json"
+});
+
+
 const allowedFileTypes = ["mp4", "mpeg4", "avi", "webm", "mov", "mpg"];
-const google = require('googleapis');
 
 
 exports.analyzeVideo = function analyzeVideo (event, callback) {
@@ -31,71 +36,33 @@ exports.analyzeVideo = function analyzeVideo (event, callback) {
 
   if (isDelete) {
     console.log(`File ${file.name} deleted.`);
-    // TODO - also delete the entity from Datastore
   } else {
     const bucketName = file.bucket;
     console.log('fileinfo: ',file);
     const fileName = file.name;
-    const videoFilename = "gs://" + bucketName + '/' + fileName;
-    console.log(videoFilename);
-
     const fileType = fileName.split('.').pop();
+
+
     if (allowedFileTypes.indexOf(fileType.toLowerCase()) != -1) {
       // it's a video!
+      const request = {
+        inputUri: "gs://" + bucketName + '/' + fileName;,
+        outputUri: "gs://" + config.video_json_bucket + "/" + fileName.replace(".", "").replace("/", "") + ".json",
+        features: ['LABEL_DETECTION', 'SHOT_CHANGE_DETECTION']
+      }
 
-      // 1) Kick off the annotation request (v1alpha1)
-      var url = 'https://videointelligence.googleapis.com/$discovery/rest'
-      var apiKey = config.video_api_key;
+      videoClient.annotateVideo(request).then(function(responses) {
+        var operation = responses[0];
+        operation.on('complete', function(result, metadata, finalResponse) {
+          console.log('got the annotations: ',result);
+        });
 
-      google.discoverAPI({
-       url: url,
-       version: 'v1beta1',
-       key: apiKey
-      }, function (serviceErr, videoService) {
-        console.log("filename is " + fileName);
-       // use the service
-        const requestBody = {
-          "auth": config.video_api_key,
-          "resource": {
-            "inputUri": videoFilename,
-            "outputUri": "gs://" + config.video_json_bucket + "/" + fileName.replace(".", "").replace("/", "") + ".json",
-            'features': ['LABEL_DETECTION', 'SHOT_CHANGE_DETECTION']
-          }
-        }
+        operation.on('progress', function(metadata,apiResp) {
+          console.log('processing: ', metadata);
+        });
 
-        // 2) Get the status of the annotations (v1)
-        videoService.videos.annotate(requestBody, function(error, resp) {
-          console.log(error);
-          const opId = resp.name;
-          console.log(opId);
-
-          var requestInterval = setInterval(function() {
-            google.discoverAPI({
-              url: url,
-              version: 'v1',
-              key: apiKey
-            }, function(serviceErr, videoIntelligence) {
-              const reqBody = {
-                "auth": config.video_api_key,
-                "name": opId
-              }
-              videoIntelligence.operations.get(reqBody, function (opErr, opResp) {
-                if (!opErr) {
-                  if (opResp['response']['annotationResults']) {
-                    const annotations = opResp['response']['annotationResults'];
-                    console.log('got the annotations!', annotations);
-                    clearInterval(requestInterval);
-                  } else {
-                    console.log('still waiting...', opResp);
-                    console.log('progress', opResp['metadata']['annotationProgress']);
-                  }
-                } else {
-                  console.log('error analyzing video', opErr);
-                }
-              });
-            })
-          }, 5000);
-
+        operation.on('error', function(err) {
+          console.log('error: ', err);
         });
       });
 
